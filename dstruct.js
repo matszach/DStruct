7f769ef9feccf9b8e10f458a71c36380bfc6b5ca
matszach <lkaszubowski@gmail.com>
2020-11-23 00:35:53 +0100

    Array2D

--- dstruct.js
+++ dstruct.js
@@ -63,6 +63,56 @@ const DStruct = {
 
         iterate(callback) {
             for(let i = 0; i < this.baseArrayLength; i++) {
+                callback(this.data[i], ...this._toPositionals(i));
+            }
+            return this;
+        }
+        
+    },
+
+
+    Array2D: class {
+        
+        constructor(width, height, arrayTypeClass) {
+            arrayTypeClass = arrayTypeClass || Array;
+            this._validateArgs(width, height, arrayTypeClass);
+            this.width = width;
+            this.height = height;
+            this.baseArrayLength = width * height;
+            this.data = new arrayTypeClass(this.baseArrayLength);
+        }
+
+        _validateArgs(width, height, arrayTypeClass) {
+            if(!width || !height) {
+                throw new Error('Width and height of array are required.');
+            }
+        }
+
+        _toIndex(x, y) {
+            return x + y * this.width;
+        }
+
+        _toPositionals(index) {
+            return new Int32Array(
+                index * this.width,
+                Math.floor(index / this.width)
+            );
+        }
+
+        get(x, y) {
+            return this.data[this._toIndex(x, y)];
+        }
+
+        put(value, x, y) {
+            return this.data[this._toIndex(x, y)] = value;
+        }
+
+        inRange(x, y) {
+            return this._toIndex(x, y) < this.baseArrayLength;
+        }
+
+        iterate(callback) {
+            for(let i = 0; i < this.baseArrayLength; i++) {
                 callback(this.data[i], this._toPositionals(i));
             }
             return this;
